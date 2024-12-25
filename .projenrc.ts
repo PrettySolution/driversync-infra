3fc0597843882d1d07d86234b36c9b82af5ed9ca
VHerman ⛅ <vasyl.herman@pretty-solution.com>
2024-12-25 14:31:54 +0200

    init

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,21 @@
 import { awscdk } from 'projen';
+import { GithubCredentials } from 'projen/lib/github';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.171.1',
   defaultReleaseBranch: 'main',
   name: 'driver-infrastructure',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  gitignore: ['.idea'],
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp(),
+  },
+  release: false,
+  deps: ['cdk-pipelines-github', 'aws-cdk-github-oidc'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: ['cdk-dia'], /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
+
