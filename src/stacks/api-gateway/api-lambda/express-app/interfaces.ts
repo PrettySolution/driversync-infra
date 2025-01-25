4058851cb782a65fb04d61192b18d6c0fa67fc3b
vasylherman ⛅ <vh@prettysolution.com>
2025-01-25 20:37:36 +0200

    fix: delete unused interfaces

--- src/stacks/api-gateway/api-lambda/express-app/interfaces.ts
+++ src/stacks/api-gateway/api-lambda/express-app/interfaces.ts
@@ -3,13 +3,3 @@ export const REPORT_TABLE_NAME: string = 'REPORT_TABLE_NAME';
 export interface AuthorizerContext {
   user: string;
 }
-
-export interface IReport {
-  timestamp: string;
-  ownerId: string;
-  type?: string;
-}
-
-export interface QueryStringParameters {
-  LastEvaluatedKey?: string;
-}
