cc11b16a2de3907f4cae85e14e802aff908807f5
vasylherman ⛅ <vh@prettysolution.com>
2025-01-30 21:14:08 +0200

    fix: set authorizerMiddleware for local dev

--- src/stacks/api-gateway/api-lambda/express-app/middlewares/authorizerMiddleware.ts
+++ src/stacks/api-gateway/api-lambda/express-app/middlewares/authorizerMiddleware.ts
@@ -3,23 +3,15 @@ import { Request, Response, NextFunction } from 'express';
 
 export function authorizerMiddleware(req: Request, res: Response, next: NextFunction): void {
   if (!req.requestContext?.authorizer?.jwt?.claims?.username && req.headers.authorization) {
-    console.error('ERROR: Authorizer middleware should not be applied in PROD');
-
-    function decodeJwtPayload(token: string): any {
-      const payload = token.split('.')[1]; // Get the payload part of the token
-      if (!payload) {
-        res.status(500).json({ error: 'Missing required header' });
-      }
-      return JSON.parse(Buffer.from(payload, 'base64').toString('utf-8')); // Decode the Base64 payload
-    }
-
+    console.log('ERROR: Authorizer middleware should not be applied in PROD');
     const claims = decodeJwtPayload(req.headers.authorization);
-
-
     req.requestContext = { authorizer: { jwt: { claims } } };
     console.log(req.requestContext);
-  } else {
-    res.status(500).json({ error: 'Missing required header' });
   }
   next();
+}
+
+function decodeJwtPayload(token: string): any {
+  const payload = token.split('.')[1]; // Get the payload part of the token
+  return JSON.parse(Buffer.from(payload, 'base64').toString('utf-8')); // Decode the Base64 payload
 }
